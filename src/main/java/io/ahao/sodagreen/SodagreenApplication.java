505d14eb05e021bd95d510566315a482ffaf48b3
wangwenhao <clovebreath9@gmail.com>
2020-07-25 14:54:24 +0800

    init

---
+++ src/main/java/io/ahao/sodagreen/SodagreenApplication.java
@@ -0,0 +1,13 @@
+package io.ahao.sodagreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodagreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodagreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ahao/sodagreen/SodagreenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ahao.sodagreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodagreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
